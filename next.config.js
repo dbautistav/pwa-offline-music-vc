40430639dff81ad510850fea4b418eaa7e8d1345
David <dbautistav@gmail.com>
2025-06-21 14:42:36 -0500

    Add GitHub actions-based deployment (from a freeCodeCamp tutorial)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages/
+    basePath: "/pwa-offline-music-vc",
+    output: "export",  // <=== enables static exports
+
     reactStrictMode: true,
     swcMinify: true,
     // Enable service worker registration
